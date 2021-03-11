a07d61a5c195c36c7caddb73fba097271c1fa9e6
Seth Forsgren <forsgren.seth@gmail.com>
2021-03-11 10:01:30 -0800

    Initial Commit

---
+++ SM-testing/SM_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SM_testingApp.swift
+//  SM-testing
+//
+//  Created by Seth Forsgren on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SM_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
